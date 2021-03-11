66632e230d70a53b9c52e3fa7e8f4e7baf45da38
之遇 曾 <zyjeng@light-year.tech>
2021-03-11 14:28:09 +0800

    seperate model

--- BulkyBook/Controllers/HomeController.cs
+++ BulkyBook/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
 
 namespace BulkyBook.Controllers
 {
--- BulkyBook/Startup.cs
+++ BulkyBook/Startup.cs
@@ -8,10 +8,10 @@ using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
-using BulkyBook.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using BulkyBook.DataAccess.Data;
 
 namespace BulkyBook
 {
